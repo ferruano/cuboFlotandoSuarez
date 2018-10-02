584277b8efd73584b026b9b3d01b1f1f7533a5c0
Saerco <saerco@macbook-pro-de-saerco.local>
2018-10-02 16:47:05 +0200

    Diseño Completo

--- Cubo_flotando/ViewController.swift
+++ Cubo_flotando/ViewController.swift
@@ -11,18 +11,37 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var LabelLado: UILabel!
+    
     @IBOutlet weak var SliderLado: UISlider!
     
     @IBOutlet weak var GraficaPosicion: ParametricFunctionView!
+    
     @IBOutlet weak var GraficaVelocidad: ParametricFunctionView!
+    
     @IBOutlet weak var GraficaAceleracion: ParametricFunctionView!
+    
     @IBOutlet weak var SliderTiempo: UISlider!
+    
     @IBOutlet weak var LabelTiempo: UILabel!
+    
+    @IBOutlet weak var GraficaTiempo: ParametricFunctionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        LabelLado.text=String(SliderLado.value) + " m"
+        LabelTiempo.text=String(SliderTiempo.value) + " seg"
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func cambioSlider(_ sender: UISlider) {
+        let t = sender.value
+        LabelLado.text = String(round(100*t)/100) + " m"
+        
+    }
+    
+    @IBAction func cambioSliderBottom(_ sender: UISlider) {
+        let t = sender.value
+        LabelTiempo.text = String(round(100*t)/100) + " seg"
+    }
 }
 
